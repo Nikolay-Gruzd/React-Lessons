40647fc0f0f4e681133dcd4bdadcbe19f7eeed6a
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-05-18 11:55:40 +0800

    Lesson 02 - Components(part 2)
    Added new components and used them to render the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import './App.css';
 
 export const App = ()=> {
+    console.log('App rendering')
     return (
         <div className="App">
-            This is App component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 
-    function Rating() { //Компонент Rating
+    function AppTitle() {
+        console.log('App Title rendering');
+        return(
+            <>
+                This is App component
+            </>
+        )
+    }
+
+    function Rating() { // Компонент Rating
+        console.log('Rating rendering')
         return (
             <div>
                 <Star/>
@@ -19,24 +31,41 @@ export const App = ()=> {
                 <Star/>
             </div>
         )
+
+        function Star() { // Компонент Star
+            console.log('Star rendering')
+            return (
+                <div>star</div>
+            )
+        }
     }
 
-    function Star() { //Компонент Star
-        return (
-            <div>star</div>
-        )
-    }
 
-    function Accordion() { //Компонент Accordion
+    function Accordion() { // Компонент Accordion
+        console.log('Accordion rendering')
         return (
             <div>
+                <AccordionTitle/>
+                <AccordionBody/>
+            </div>
+        )
+
+        function AccordionTitle() { // Компонент AccordionTitle
+            console.log('AccordionTitle rendering')
+            return (
                 <h3>Меню</h3>
+            )
+        }
+
+        function AccordionBody() { // Компонент AccordionBody
+            console.log('AccordionBody rendering')
+            return (
                 <ul>
                     <li>1</li>
                     <li>2</li>
                     <li>3</li>
                 </ul>
-            </div>
-        )
+            )
+        }
     }
 }
