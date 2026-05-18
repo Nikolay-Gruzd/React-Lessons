16427da053dd3569002e99c1bbfaa5a718345323
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-05-18 11:33:48 +0800

    Lesson 01 - Components(part 1)
    Added the Rating, Star, and Accordion components and used them to render the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,40 @@ import './App.css';
 export const App = ()=> {
     return (
         <div className="App">
-            Привет
+            This is App component
+            <Rating/>
+            <Accordion/>
         </div>
     );
+
+    function Rating() { //Компонент Rating
+        return (
+            <div>
+                <Star/>
+                <Star/>
+                <Star/>
+                <Star/>
+                <Star/>
+            </div>
+        )
+    }
+
+    function Star() { //Компонент Star
+        return (
+            <div>star</div>
+        )
+    }
+
+    function Accordion() { //Компонент Accordion
+        return (
+            <div>
+                <h3>Меню</h3>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+            </div>
+        )
+    }
 }
