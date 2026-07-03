32395ed20edf347e9537e9c44178ee4bac9fda15
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-07-03 18:26:45 +0800

    Lesson 10.1 - Installing and configuring Storybook

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+// .eslintrc.cjs
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'storybook-static'],
+    parser: '@typescript-eslint/parser',  // ✅ КЛЮЧЕВОЕ
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',  // ✅ КЛЮЧЕВОЕ
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+    },
+    overrides: [
+        {
+            files: ['vite.config.ts', '**/*.stories.tsx', '.storybook/**/*'],
+            rules: {
+                'react-refresh/only-export-components': 'off',
+            },
+        },
+    ],
+};
