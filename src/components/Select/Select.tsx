cf2b3a267d126706be766c9736b33bb6f56b7c1d
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-07-27 23:31:43 +0800

    Lesson 15 - Custom Select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,20 +1,65 @@
+import s from './Select.module.css'
+import {useState, KeyboardEvent} from "react";
+
 type ItemType = {
     title: string
     value: any
 }
 type SelectType = {
-    value: any
+    value?: any
     onChange: (value: any) => void
     items: Array<ItemType>
 }
 
 export function Select(props: SelectType) { // Компонент ControlledSelect
-    console.log('ControlledSelect rendering')
+
+    const [active, setActive] = useState(false)
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value)
+
+    const selectedItem = props.items.find(i => i.value === props.value)
+    const hoveredItem = props.items.find(i => i.value === hoveredElementValue)
+
+    const toggleItems = () => {
+        setActive(!active)
+        setHoveredElementValue(props.value)
+    }
+    const onItemClick = (value: any) => {
+        props.onChange(value);
+        toggleItems();
+    }
+    const onMouseEnter = (value: any) => {
+        setHoveredElementValue(value)
+    }
+    const onKeyUp = (e: KeyboardEvent<HTMLSpanElement>) => {
+        console.log('press')
+    }
 
     return (
-        <div>
-            <div>{}</div>
-            {props.items.map((i, index) => <div key={index}>{i.title}</div>)}
-        </div>
+        <>
+            <div className={s.select}>
+                <span className={s.main}
+                      onClick={toggleItems}
+                      onKeyUp={onKeyUp}
+                      tabIndex={0}
+                >
+                    {selectedItem && selectedItem.title}
+                </span>
+                {active &&
+                    <div className={s.items}>
+                        {props.items.map(i => (
+                            <div onClick={() => {
+                                onItemClick(i.value)
+                            }}
+                                 onMouseEnter={() => onMouseEnter(i.value)}
+                                 className={s.item + " " + (hoveredItem === i ? s.selected : '')}
+                                 key={i.value}
+                            >
+                                {i.title}
+                            </div>
+                        ))}
+                    </div>
+                }
+            </div>
+        </>
     )
 }
