5a6017cea6103168dca051fdd1995d66924de542
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-08-30 16:45:34 +0800

    Lesson 22 - useEffect with setTimeout and setInterval;

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState, useRef} from "react";
+import {useEffect, useState} from "react";
 
 export default {
     title: 'useEffect demo'
@@ -41,3 +41,60 @@ export const UseEffectExample = () => {
     )
 }
 
+export const SetTimeoutExample = () => {
+    console.log('SetTimeOutExample')
+
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+
+    useEffect(() => {
+        // setTimeout(() => {
+        //     console.log('setTimeout')
+        //     document.title = counter.toString()
+        // }, 1000)
+
+        setInterval(() => {
+            console.log('setInterval')
+            setCounter(counter + 1)
+        }, 1000)
+    }, [counter]);
+
+
+
+    return (
+        <>
+            Hello, counter: {counter} - fake: {fake}
+            <button onClick={() => setCounter(counter + 1)}>+</button>
+            <button onClick={() => setFake(fake + 1)}>+++</button>
+        </>
+    )
+}
+
+export const watchExample = () => {
+    console.log('watchExample')
+
+    const [hours, setHours] = useState(16)
+    const [minutes, setMinutes] = useState(59)
+    const [seconds, setSeconds] = useState(50)
+
+    useEffect(() => {
+        setInterval(() => {
+            setSeconds(seconds + 1)
+            if (seconds === 59) {
+                setSeconds(0)
+            }
+        }, 1000)
+    }, [seconds]);
+
+    useEffect(() => {
+
+    }, [minutes]);
+
+
+
+    return (
+        <>
+            Time: {hours}:{minutes}:{seconds}
+        </>
+    )
+}
